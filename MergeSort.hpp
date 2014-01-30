1c9ef12a3d985d5bdbf6dc40ffe07cd1b217b600
Miguel Angel Aguirre Raffoul <magraffoul@gmail.com>
2014-01-29 23:49:10 -0600

    Merge

--- MergeSort.hpp
+++ MergeSort.hpp
@@ -7,8 +7,41 @@
 void mergeArrays( std::vector<int>& my_vector, std::vector<int>& aux_vector,
                   int start, int mid, int end )
 {
-    int s = start;
-    int m = mid + 1;
+    int index = start;
+    int subar1_index = start;
+    int subar2_index = mid + 1;
+    while( subar1_index <= mid  && subar2_index <= end  )
+    {
+        if( my_vector[subar1_index] <= my_vector[subar2_index] )
+        {
+            aux_vector[index] = my_vector[subar1_index];
+            ++subar1_index;
+        }
+        else
+        {
+            aux_vector[index] = my_vector[subar2_index];
+            ++subar2_index;
+        }
+        ++index;
+    }
+    if( subar1_index <= mid )
+    {
+        while( subar1_index <= mid )
+        {
+            aux_vector[index] = my_vector[subar1_index];
+            ++index;
+            ++subar1_index;
+        }
+    }
+    else
+    {
+        while( subar2_index <= end )
+        {
+            aux_vector[index] = my_vector[subar2_index];
+            ++index;
+            ++subar2_index;
+        }
+    }
     for( int i = start ; i <= end; ++i )
         my_vector[i] = aux_vector[i];
 }
